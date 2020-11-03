ebb951732b1a94e2f8df5ea0c1b840ba2586d33f
Konstantinos <kongallis@gmail.com>
2020-11-03 15:06:39 +0200

    Initial commit

---
+++ src/test/java/com/kongallis/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kongallis.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
