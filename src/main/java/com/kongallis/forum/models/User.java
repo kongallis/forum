a256e0df3b6ec198ec1bf8fbe2302157574415e5
Konstantinos <kongallis@gmail.com>
2020-11-03 20:35:00 +0200

    Created the User entity

---
+++ src/main/java/com/kongallis/forum/models/User.java
@@ -0,0 +1,91 @@
+package com.kongallis.forum.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String firstName;
+    @NotBlank
+    private String lastName;
+    @NotBlank
+    private String userName;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String password;
+    @NotBlank
+    private String email;
+    private String avatar;
+
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    // Implement getter for the Full Name
+    public String getName() {
+        return String.format("%s %s", getFirstName(), getLastName());
+    }
+
+
+}
