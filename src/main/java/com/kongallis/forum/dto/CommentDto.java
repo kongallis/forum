383648547a6bbbcb1aaf423a4b21a797e48a3db6
Konstantinos <kongallis@gmail.com>
2020-11-05 12:20:38 +0200

    Created CommentDto

---
+++ src/main/java/com/kongallis/forum/dto/CommentDto.java
@@ -0,0 +1,72 @@
+package com.kongallis.forum.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Date;
+
+public class CommentDto {
+
+    private Long commentId;
+    private Long postId;
+    private Date createdAt;
+    private String email;
+    private String avatar;
+    private String name;
+    private String body;
+
+    public Long getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(Long commentId) {
+        this.commentId = commentId;
+    }
+
+    public Long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
